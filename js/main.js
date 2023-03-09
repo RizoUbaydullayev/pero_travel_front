02d22c089fc26742d576fd459bebcd2ea6ebbf1a
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-03-09 23:17:21 +0500

    Header ready

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const header_slider = new Swiper('.header_slider', {
+    wrapperClass: "header_slider_wrap",
+    slideClass: "page",
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+})
