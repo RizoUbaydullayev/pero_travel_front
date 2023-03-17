43cd09fa9921a70c38fb77284b5678d958f39007
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-03-17 22:58:25 +0500

    Gallery ready

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
 /*=========================  header slider  ================================================*/
 
 const header_slider = new Swiper('.header_slider', {
-    wrapperClass: "header_slider_wrap",
-    slideClass: "page",
-    slidesPerView: 4,
-    spaceBetween: 20,
-    loop: true,
+	wrapperClass: "header_slider_wrap",
+	slideClass: "page",
+	slidesPerView: 4,
+	spaceBetween: 20,
+	loop: true,
 })
 
 /*=========================  popular excursions slider  ================================================*/
 
 const popular_excursions_slider = new Swiper('.popular_excursions_slider', {
-    wrapperClass: "popular_excursions_slider_wrap",
-    slideClass: "page",
-    slidesPerView: 5.1,
-    spaceBetween: 20,
-    freeMode: {
-        enabled: true,
-        sticky: false,
-    },
-    pagination: {
-        el: ".progress_line",
-        type: "progressbar",
-        progressbarOpposite: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+	wrapperClass: "popular_excursions_slider_wrap",
+	slideClass: "page",
+	slidesPerView: 5.1,
+	spaceBetween: 20,
+	freeMode: {
+		enabled: true,
+		sticky: false,
+	},
+	pagination: {
+		el: ".progress_line",
+		type: "progressbar",
+		progressbarOpposite: false,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 })
 
 /*=========================  galery  ================================================*/
 
-$(document).ready(function() {
-	$('.gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
+let pop_up_for_gallery = document.querySelector(".pop_up_for_gallery");
+let pop_up_img = pop_up_for_gallery.children[1].children[0];
+let gallery_images = document.querySelector(".gallery > .gallery_images");
+document.addEventListener("click", event => {
+	if (event.target == pop_up_img ||
+		event.target == pop_up_for_gallery.children[1]) {
+		pop_up_for_gallery.style.cssText = "display: flex;";
 		
-	});
-});
+	}
+	else {
+		gallery_images.style.cssText = "opacity: 1;";
+		pop_up_for_gallery.style.cssText = "display: none;";
+	}
+})
+
+document.addEventListener("click", event => {
+	if (event.target.classList.contains("gallery_image")) {
+		console.log(document.querySelector("body"));
+		src_for_popup = window.getComputedStyle(event.target).backgroundImage;
+		pop_up_for_gallery.style.cssText = "display: flex;";
+		gallery_images.style.cssText = "opacity: 0.3;";
+		pop_up_img.setAttribute("src", `./${src_for_popup.slice(src_for_popup.indexOf("img"), -2)}`);
+	}
+})
+
+
