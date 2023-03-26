407ddd088d2ec16b42113799de6dcb210bf4f89f
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-03-26 13:16:19 +0500

    adaptive section popular_excursions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*=========================  header slider  ================================================*/
 
-const header_slider = new Swiper('.header_slider', {
+let header_slider = new Swiper('.header_slider', {
 	wrapperClass: "header_slider_wrap",
 	slideClass: "page",
 	slidesPerView: 4,
@@ -30,51 +30,120 @@ const popular_excursions_slider = new Swiper('.popular_excursions_slider', {
 	},
 })
 
-/*=========================  galery  ================================================*/
+// /*=========================  galery  ================================================*/
 
-let pop_up_for_gallery = document.querySelector(".pop_up_for_gallery");
-let pop_up_img = pop_up_for_gallery.children[1].children[0];
-let gallery_images = document.querySelector(".gallery > .gallery_images");
-document.addEventListener("click", event => {
-	if (event.target == pop_up_img ||
-		event.target == pop_up_for_gallery.children[1]) {
-		pop_up_for_gallery.style.cssText = "display: flex;";
-		
-	}
-	else {
-		gallery_images.style.cssText = "opacity: 1;";
-		pop_up_for_gallery.style.cssText = "display: none;";
-	}
-})
+// let pop_up_for_gallery = document.querySelector(".pop_up_for_gallery");
+// let pop_up_img = pop_up_for_gallery.children[1].children[0];
+// let gallery_images = document.querySelector(".gallery > .gallery_images");
+// document.addEventListener("click", event => {
+// 	if (event.target == pop_up_img ||
+// 		event.target == pop_up_for_gallery.children[1]) {
+// 		pop_up_for_gallery.style.cssText = "display: flex;";
 
-document.addEventListener("click", event => {
-	if (event.target.classList.contains("gallery_image")) {
-		console.log(document.querySelector("body"));
-		src_for_popup = window.getComputedStyle(event.target).backgroundImage;
-		pop_up_for_gallery.style.cssText = "display: flex;";
-		gallery_images.style.cssText = "opacity: 0.3;";
-		pop_up_img.setAttribute("src", `./${src_for_popup.slice(src_for_popup.indexOf("img"), -2)}`);
-	}
-})
+// 	}
+// 	else {
+// 		gallery_images.style.cssText = "opacity: 1;";
+// 		pop_up_for_gallery.style.cssText = "display: none;";
+// 	}
+// })
 
-/*=========================  reviews  ================================================*/
-const reviews_slider = new Swiper('.reviews_slider', {
-	wrapperClass: "reviews_slider_wrap",
-	slideClass: "page",
-	slidesPerView: 2.3,
-	// spaceBetween: 20,
-	freeMode: {
-		enabled: true,
-		sticky: false,
-	},
-	pagination: {
-		el: ".progress_line",
-		type: "progressbar",
-		progressbarOpposite: false,
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-})
+// document.addEventListener("click", event => {
+// 	if (event.target.classList.contains("gallery_image")) {
+// 		console.log(document.querySelector("body"));
+// 		src_for_popup = window.getComputedStyle(event.target).backgroundImage;
+// 		pop_up_for_gallery.style.cssText = "display: flex;";
+// 		gallery_images.style.cssText = "opacity: 0.3;";
+// 		pop_up_img.setAttribute("src", `./${src_for_popup.slice(src_for_popup.indexOf("img"), -2)}`);
+// 	}
+// })
+
+// /*=========================  reviews  ================================================*/
+
+// const reviews_slider = new Swiper('.reviews_slider', {
+// 	wrapperClass: "reviews_slider_wrap",
+// 	slideClass: "page",
+// 	slidesPerView: 2.3,
+// 	// spaceBetween: 20,
+// 	freeMode: {
+// 		enabled: true,
+// 		sticky: false,
+// 	},
+// 	pagination: {
+// 		el: ".progress_line",
+// 		type: "progressbar",
+// 		progressbarOpposite: false,
+// 	},
+// 	navigation: {
+// 		nextEl: ".swiper-button-next",
+// 		prevEl: ".swiper-button-prev",
+// 	},
+// })
+
+// /*=========================  Адаптив  ================================================*/
+
+let width = window.innerWidth;
+adaptive(width);
+
+
+function adaptive(width) {
+	if (width > 1920) {
+		header_slider.params.slidesPerView = 5.2;
+
+	};
+
+	if (width > 1600 && width <= 1920) {
+		header_slider.params.slidesPerView = 4;
+		popular_excursions_slider.params.slidesPerView = 5.1;
+		popular_excursions_slider.params.spaceBetween = 20;
+
+	};
+
+	if (width >= 1280 && width <= 1600) {
+		header_slider.params.slidesPerView = 4;
+		popular_excursions_slider.params.slidesPerView = 4;
+		popular_excursions_slider.params.spaceBetween = 20;
+
+	};
+
+	if (width >= 1024 && width < 1280) {
+		header_slider.params.slidesPerView = 3;
+		popular_excursions_slider.params.slidesPerView = 3.5;
+		popular_excursions_slider.params.spaceBetween = 20;
+
+	};
+
+	if (width >= 768 && width < 1024) {
+		header_slider.params.slidesPerView = 3;
+		popular_excursions_slider.params.slidesPerView = 2.8;
+		popular_excursions_slider.params.spaceBetween = 10;
+	};
+
+	if (width >= 480 && width < 768) {
+		header_slider.params.slidesPerView = 1.5;
+		popular_excursions_slider.params.slidesPerView = 2.1;
+		popular_excursions_slider.params.spaceBetween = 10;
+		if (width < 550) {
+			popular_excursions_slider.params.slidesPerView = 1.5;
+
+		}
+
+	};
+	if (width >= 320 && width < 480) {
+		header_slider.params.slidesPerView = 1.5;
+		popular_excursions_slider.params.slidesPerView = 1.3;
+		popular_excursions_slider.params.spaceBetween = 10;
+		if (width < 350) {
+			popular_excursions_slider.params.slidesPerView = 1.2;
+
+		}
+	};
+};
+window.addEventListener("resize", event => {
+	width = window.innerWidth;
+	adaptive(width);
+});
+
+
+
+
 
