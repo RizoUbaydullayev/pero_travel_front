9790fbf317f0b17cc800daa7abc2344c02140a6d
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-04-05 02:27:25 +0500

    section review adaptive

--- js/main.js
+++ js/main.js
@@ -30,54 +30,54 @@ const popular_excursions_slider = new Swiper('.popular_excursions_slider', {
 	},
 })
 
-// /*=========================  galery  ================================================*/
+/*=========================  galery  ================================================*/
 
-// let pop_up_for_gallery = document.querySelector(".pop_up_for_gallery");
-// let pop_up_img = pop_up_for_gallery.children[1].children[0];
-// let gallery_images = document.querySelector(".gallery > .gallery_images");
-// document.addEventListener("click", event => {
-// 	if (event.target == pop_up_img ||
-// 		event.target == pop_up_for_gallery.children[1]) {
-// 		pop_up_for_gallery.style.cssText = "display: flex;";
+let pop_up_for_gallery = document.querySelector(".pop_up_for_gallery");
+let pop_up_img = pop_up_for_gallery.children[0].children[1];
+let gallery_images = document.querySelector(".gallery > .gallery_images");
+document.addEventListener("click", event => {
+	if (event.target == pop_up_img ||
+		event.target == pop_up_for_gallery.children[1]) {
+		pop_up_for_gallery.style.cssText = "display: flex;";
 
-// 	}
-// 	else {
-// 		gallery_images.style.cssText = "opacity: 1;";
-// 		pop_up_for_gallery.style.cssText = "display: none;";
-// 	}
-// })
+	}
+	else {
+		gallery_images.style.cssText = "opacity: 1;";
+		pop_up_for_gallery.style.cssText = "display: none;";
+	}
+})
 
-// document.addEventListener("click", event => {
-// 	if (event.target.classList.contains("gallery_image")) {
-// 		console.log(document.querySelector("body"));
-// 		src_for_popup = window.getComputedStyle(event.target).backgroundImage;
-// 		pop_up_for_gallery.style.cssText = "display: flex;";
-// 		gallery_images.style.cssText = "opacity: 0.3;";
-// 		pop_up_img.setAttribute("src", `./${src_for_popup.slice(src_for_popup.indexOf("img"), -2)}`);
-// 	}
-// })
+document.addEventListener("click", event => {
+	if (event.target.classList.contains("gallery_image")) {
+		console.log(document.querySelector("body"));
+		src_for_popup = window.getComputedStyle(event.target).backgroundImage;
+		pop_up_for_gallery.style.cssText = "display: flex;";
+		gallery_images.style.cssText = "opacity: 0.3;";
+		pop_up_img.setAttribute("src", `./${src_for_popup.slice(src_for_popup.indexOf("img"), -2)}`);
+	}
+})
 
-// /*=========================  reviews  ================================================*/
+/*=========================  reviews  ================================================*/
 
-// const reviews_slider = new Swiper('.reviews_slider', {
-// 	wrapperClass: "reviews_slider_wrap",
-// 	slideClass: "page",
-// 	slidesPerView: 2.3,
-// 	// spaceBetween: 20,
-// 	freeMode: {
-// 		enabled: true,
-// 		sticky: false,
-// 	},
-// 	pagination: {
-// 		el: ".progress_line",
-// 		type: "progressbar",
-// 		progressbarOpposite: false,
-// 	},
-// 	navigation: {
-// 		nextEl: ".swiper-button-next",
-// 		prevEl: ".swiper-button-prev",
-// 	},
-// })
+const reviews_slider = new Swiper('.reviews_slider', {
+	wrapperClass: "reviews_slider_wrap",
+	slideClass: "page",
+	slidesPerView: 2.3,
+	spaceBetween: 20,
+	freeMode: {
+		enabled: true,
+		sticky: false,
+	},
+	pagination: {
+		el: ".progress_line",
+		type: "progressbar",
+		progressbarOpposite: false,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+})
 
 // /*=========================  Адаптив  ================================================*/
 
@@ -88,6 +88,7 @@ adaptive(width);
 function adaptive(width) {
 	if (width > 1920) {
 		header_slider.params.slidesPerView = 5.2;
+		reviews_slider.params.slidesPerView = 3.1;
 
 	};
 
@@ -95,6 +96,7 @@ function adaptive(width) {
 		header_slider.params.slidesPerView = 4;
 		popular_excursions_slider.params.slidesPerView = 5.1;
 		popular_excursions_slider.params.spaceBetween = 20;
+		reviews_slider.params.slidesPerView = 2.3;
 
 	};
 
@@ -102,13 +104,14 @@ function adaptive(width) {
 		header_slider.params.slidesPerView = 4;
 		popular_excursions_slider.params.slidesPerView = 4;
 		popular_excursions_slider.params.spaceBetween = 20;
-
+		reviews_slider.params.slidesPerView = 2;
 	};
 
 	if (width >= 1024 && width < 1280) {
 		header_slider.params.slidesPerView = 3;
 		popular_excursions_slider.params.slidesPerView = 3.5;
 		popular_excursions_slider.params.spaceBetween = 20;
+		reviews_slider.params.slidesPerView = 1.5;
 
 	};
 
@@ -116,6 +119,7 @@ function adaptive(width) {
 		header_slider.params.slidesPerView = 3;
 		popular_excursions_slider.params.slidesPerView = 2.8;
 		popular_excursions_slider.params.spaceBetween = 10;
+		reviews_slider.params.slidesPerView = 1.2;
 	};
 
 	if (width >= 480 && width < 768) {
@@ -126,6 +130,7 @@ function adaptive(width) {
 			popular_excursions_slider.params.slidesPerView = 1.5;
 
 		}
+		reviews_slider.params.slidesPerView = 1;
 
 	};
 	if (width >= 320 && width < 480) {
@@ -136,6 +141,7 @@ function adaptive(width) {
 			popular_excursions_slider.params.slidesPerView = 1.2;
 
 		}
+		reviews_slider.params.slidesPerView = 1.3;
 	};
 };
 window.addEventListener("resize", event => {
