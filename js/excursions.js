46e8c1a60c21149913fa89a88dbe3e504da548a9
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-05-14 21:40:37 +0500

    page excursions ready

--- js/excursions.js
+++ js/excursions.js
@@ -1,19 +1,27 @@
 // =============== types_of_excursions-tabs ===============
 
-// pathname = window.location.pathname;
-// let tabs_list = document.querySelector(".tabs_list");
-// for (let i=0; i<tabs_list.children.length; i++) {
-//     if (tabs_list.children[i].children[0].getAttribute("href") == pathname) {
-//         tabs_list.children[i].classList.add("active");
-//     };
-// };
+pathname = window.location.pathname;
+let tabs_list = document.querySelector(".tabs_list");
+for (let i = 0; i < tabs_list.children.length; i++) {
+    if (tabs_list.children[i].children[0].getAttribute("href") == pathname) {
+        tabs_list.children[i].classList.add("active");
+    };
+};
 
 // =============== small_filter ===================================
 
-new AirDatepicker('.date-input', {
+let smallFilterDateInput = new AirDatepicker('.date-input', {
     multipleDates: true,
-
+    buttons: ['clear']
 });
+
+
+if (window.innerWidth <= 768) {
+    smallFilterDateInput.update(
+        { isMobile: true }
+    )
+}
+
 
 // =============== excursion_main =================================
 
@@ -33,7 +41,10 @@ filter_elements.forEach(element => {
 
 
 bigFilterDataValue = new AirDatepicker('.big_filter_data_value', {
+    buttons: ['clear']
 });
+
+
 
 addToFavoritesBtn = document.querySelector("#add_to_favorites");
 addToFavoritesBtnLabels = document.querySelectorAll(".add_to_favorites_label");
