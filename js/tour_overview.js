40eccf710ac7b347592bbab5817b646a5ce05209
RizoUbaydullayev <rizoubaydullayev17@gmail.com>
2023-06-07 11:02:35 +0500

    section with calendar ready

--- js/tour_overview.js
+++ js/tour_overview.js
@@ -1,8 +1,31 @@
 // =============== header =================================
-addToFavoritesBtn = document.querySelector("#add_to_favorites");
-addToFavoritesBtnLabels = document.querySelectorAll(".add_to_favorites_label");
+let addToFavoritesBtn = document.querySelector("#add_to_favorites");
+let addToFavoritesBtnLabels = document.querySelectorAll(".add_to_favorites_label");
 addToFavoritesBtnLabels.forEach(label => {
     label.addEventListener("click", (event) => {
         event.target.classList.toggle("label_checked");
     })
 })
+
+// =============== travel_calendar =================================
+let travelCalendar = new AirDatepicker('.travel_calendar-calendar', {
+    classes: "air_datepicker-travel_calendar",
+
+});
+
+let calendarDays = document.querySelectorAll(".-day-");
+
+calendarDays.forEach(el => {
+    if (!el.classList.contains('-other-month-')) {  
+        let startTime = "6:00";
+        let endTime = "20:00";
+        el.innerHTML = `
+        <div class="day_inner">
+            <div>${el.textContent}</div>
+            <div class="tour_time">${startTime}:${endTime}</div>
+        </div>`
+    }
+})
+
+
+
